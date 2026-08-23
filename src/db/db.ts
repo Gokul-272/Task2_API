9f4cd8b1a488b809c3826ed6062a08261a25424a
Gokul <gokulvm2005@gamil.com>
2026-08-23 19:18:52 +0530

    change

--- src/db/db.ts
+++ src/db/db.ts
@@ -2,8 +2,10 @@ import "dotenv/config";
 import mongoose from "mongoose";
 export default async function connectDB() {
   try {
+    console.log("NODE_ENV:", process.env.NODE_ENV);
+    console.log("mongodb_url exists:", !!process.env.mongodb_url);
     await mongoose.connect(process.env.mongodb_url as string);
-    console.log("Connected to MongoDB");
+    console.log("MongoDB connected:", mongoose.connection.readyState);
   } catch (error) {
     console.error("Error connecting to MongoDB:", error);
   }
